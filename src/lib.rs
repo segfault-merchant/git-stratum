66a030e173f93c0932d7ce944bf6371f87526d54
Jordan <165378667+jordan-314@users.noreply.github.com>
2026-05-01 19:08:51 +0000

    update(test): Unitise test repo generation to ensure readability

--- src/lib.rs
+++ src/lib.rs
@@ -58,29 +58,67 @@ mod common {
     pub const EXPECTED_ACTOR_NAME: &str = "test";
     pub const EXPECTED_ACTOR_EMAIL: &str = "test@example.com";
 
-    /// Make a repository a very basic histroy
-    fn make_repo(tmpdir: &TempDir) {
-        let repo = git2::Repository::init(tmpdir.path()).expect("Failed to init repo");
+    /// Write to a file that exists within a temp directory root
+    fn write_fp(root: &TempDir, path: &str, content: &str) {
+        let fp = root.path().join(path);
+        fs::write(&fp, content).expect("Failed to write to file");
+    }
 
-        let fp = tmpdir.path().join("file.txt");
-        fs::write(&fp, "Hello World\n").expect("Failed to write to file");
-
-        // Stage file for commit
-        let mut index = repo.index().expect("Failed to get index");
+    /// Write a file to a git index
+    fn write_to_index(index: &mut git2::Index, file: &str) {
         index
-            .add_path(Path::new("file.txt"))
+            .add_path(Path::new(file))
             .expect("Failed to add file to index");
         index.write().expect("Failed to write index");
+    }
+
+    /// Write an index to a repository tree
+    fn write_tree<'a>(
+        repo: &'a git2::Repository,
+        index: &mut git2::Index,
+        file: &str,
+    ) -> git2::Tree<'a> {
+        write_to_index(index, file);
 
         let tree_id = index.write_tree().expect("Failed to write tree");
-        let tree = repo.find_tree(tree_id).expect("Failed to find tree");
+        repo.find_tree(tree_id).expect("Failed to find tree")
+    }
 
-        // Define author and committer local to this repo
+    /// Commit a file that has been modified
+    fn commit_file(
+        repo: &git2::Repository,
+        sig: &git2::Signature,
+        file: &str,
+        parent: Option<&git2::Commit<'_>>,
+    ) -> git2::Oid {
+        // Stage file for commit
+        let mut index = repo.index().expect("Failed to get index");
+        let tree = write_tree(repo, &mut index, file);
+
+        let parents = match parent {
+            Some(v) => vec![v],
+            None => vec![],
+        };
+
+        repo.commit(Some("HEAD"), sig, sig, EXPECTED_MSG, &tree, &parents)
+            .expect("Failed to create commit")
+    }
+
+    /// Make a repository with a very basic histroy.
+    fn make_repo(tmpdir: &TempDir) {
+        let repo = git2::Repository::init(tmpdir.path()).expect("Failed to init repo");
         let sig = git2::Signature::now(EXPECTED_ACTOR_NAME, EXPECTED_ACTOR_EMAIL)
             .expect("Failed to create actor signature");
 
-        repo.commit(Some("HEAD"), &sig, &sig, EXPECTED_MSG, &tree, &[])
-            .expect("Failed to create commit");
+        let file = "file.txt";
+        write_fp(tmpdir, file, "Hello World\n");
+        let first_commit_id = commit_file(&repo, &sig, file, None);
+
+        write_fp(tmpdir, file, "Hello World\nFile Update\n");
+        let parent = repo
+            .find_commit(first_commit_id)
+            .expect("Failed to find first commit");
+        commit_file(&repo, &sig, file, Some(&parent));
     }
 
     /// Lazily construct the test data into a temp dir that will last the length of
@@ -97,6 +135,27 @@ mod common {
     }
 
     /// Init a repository object using the lazily constructed git2 repo
+    ///
+    /// The repository exists in a temporary diectory and is made with some
+    /// expected values that are public constants.
+    ///
+    /// The commit history is two commits long, two commits so that 2/3 of the
+    /// diff creation methods can be tested, this removes the need to mock
+    /// git2::Diff, which may not even be possible.
+    ///
+    /// ## Commit 1
+    ///
+    /// - One file was added, `file.txt`
+    ///     - The string "Hello World\n" was written to this file
+    /// - The commit is authored and committed by: test <test@example.com>
+    /// - The commit message is: "commit msg"
+    ///
+    /// ## Commit 2
+    ///
+    /// - The same file, file.txt is updated
+    ///     - The file now contains the string "Hello World\nFile Update\n"
+    /// - The commit is authored and committed by: test <test@example.com>
+    /// - The commit message is: "commit msg"
     pub fn init_repo() -> Repository<Local> {
         Repository::<Local>::new(test_data_dir()).expect("Failed to init local repository")
     }
